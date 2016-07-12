d00e7acc1e07eb83a3e48d1af7f3f32a0f07e93d
Laxmi Tulasi Vaddiapti <laxmi.vaddipati@valuelabs.com>
2016-07-12 16:12:39 +0530

    initial commit

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			comments List
+			<ul>
+				<li>comment1</li>
+				<li>comment2</li>
+				<li>comment3</li>
+			</ul>
+		</div>
+	)
+}
